2f57fcb027368018f983fc7a8c6ad06b70f5e64f
Jhonas <jhonas.moura@tidewise.io>
2022-12-08 14:30:14 -0300

    Initial commit

---
+++ network_status_monitoringTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef network_status_monitoring_TYPES_HPP
+#define network_status_monitoring_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace network_status_monitoring {
+}
+
+#endif
+
